e322bb3e27902c791734c350969475d513bde282
najminurin <abdullahnajmi12@gmail.com>
2025-12-25 16:01:59 +0800

    comit

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,13 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('home');
+});
+Route::get('/about', function () {
     return view('welcome');
 });
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
